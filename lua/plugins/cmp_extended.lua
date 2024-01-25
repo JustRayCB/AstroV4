def92087d0675273888e882f418f5d406501ef5f
JustRayCB <rayancbpro@gmail.com>
2024-01-25 11:58:32 +0100

    Extend CMP configuration

---
+++ lua/plugins/cmp_extended.lua
@@ -0,0 +1,51 @@
+return {
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    "hrsh7th/cmp-calc",
+    "hrsh7th/cmp-emoji",
+    "jc-doyle/cmp-pandoc-references",
+    "kdheepak/cmp-latex-symbols",
+    "mstanciu552/cmp-octave",
+  },
+  opts = function(_, opts)
+    -- opts parameter is the default options table
+    -- the function is lazy loaded so cmp is able to be required
+    local cmp = require "cmp"
+    local luasnip = require "luasnip"
+    -- To  fix the issue of tab jumping around "randomly"
+    luasnip.config.set_config { -- To fix the issue of tab jumping around "randomly"
+      region_check_events = "InsertEnter",
+      delete_check_events = "InsertLeave",
+      -- update_events = {"TextChanged", "TextChangedI"}
+    }
+    -- modify the mapping part of the table
+    opts.mapping["<CR>"] = cmp.mapping.confirm { select = true }
+    opts.mapping["<C-x>"] = cmp.mapping.select_next_item()
+    opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
+      if luasnip.expand_or_locally_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { "i", "s" })
+    opts.mapping["<C-p>"] = cmp.mapping(function()
+      if luasnip.jumpable(-1) then luasnip.jump(-1) end
+    end, { "i", "s" })
+    opts.mapping["<C-j>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select } -- to fix the c-j not working since update 2/05/23
+    opts.mapping["<C-k>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select }
+
+    -- Setup sources priority
+    opts.sources = cmp.config.sources {
+      { name = "luasnip", priority = 1750 },
+      { name = "nvim_lsp", priority = 1000 },
+      -- { name = "vim-dadbod-completion", priority = 800 },
+      { name = "cmp_octave", priority = 800 },
+      { name = "pandoc_references", priority = 725 },
+      { name = "latex_symbols", priority = 700 },
+      { name = "emoji", priority = 700 },
+      { name = "calc", priority = 650 },
+      { name = "path", priority = 500 },
+      { name = "buffer", priority = 250 },
+    }
+  end,
+}
