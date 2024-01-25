e7657f405d2c781dc5374120810cb3a337b57055
JustRayCB <rayancbpro@gmail.com>
2024-01-25 11:59:14 +0100

    Extend telescope config with media file but does not work

---
+++ lua/plugins/telescope_extended.lua
@@ -0,0 +1,24 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  dependencies = { -- add a new dependency to telescope that is our new plugin
+    "nvim-telescope/telescope-media-files.nvim",
+  },
+  -- the first parameter is the plugin specification
+  -- the second is the table of options as set up in Lazy with the `opts` key
+  config = function(plugin, opts)
+    -- run the core AstroNvim configuration function with the options table
+    require "astronvim.plugins.configs.telescope"(plugin, opts)
+
+    -- require telescope and load extensions as necessary
+    require("telescope").load_extension "media_files"
+    -- opts.extensions = {
+    --   media_files = {
+    --     -- filetypes whitelist
+    --     -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
+    --     filetypes = { "png", "webp", "jpg", "jpeg", "pdf" },
+    --     -- find command (defaults to `fd`)
+    --     find_cmd = "rg",
+    --   },
+    -- }
+  end,
+}
