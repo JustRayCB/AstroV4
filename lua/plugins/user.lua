57cb0ed57d87144856817e0564258d34d2a67433
JustRayCB <rayancbpro@gmail.com>
2024-02-19 14:48:34 +0100

    Always highlligh todo comments

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -16,6 +16,9 @@ return {
         -- your configuration comes here
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
+        highlight = {
+          comments_only = false,
+        },
       }
     end,
     -- opts = {},
