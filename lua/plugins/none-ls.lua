d43c58d84432e5376444ad25233034f1c5ed7d4e
JustRayCB <rayancbpro@gmail.com>
2024-03-01 00:28:17 +0100

    None-ls refractor + checkstyle

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -6,7 +6,7 @@ return {
   "nvimtools/none-ls.nvim",
   opts = function(_, config)
     -- config variable is the default configuration table for the setup function call
-    -- local null_ls = require "null-ls"
+    local null_ls = require "null-ls"
 
     -- Check supported formatters and linters
     -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -15,6 +15,12 @@ return {
       -- Set a formatter
       -- null_ls.builtins.formatting.stylua,
       -- null_ls.builtins.formatting.prettier,
+      -- null_ls.builtins.diagnostics.checkstyle.with {
+      --   extra_args = { "-c", "/google_checks.xml" }, -- or "/sun_checks.xml" or path to self written rules
+      -- },
+      null_ls.builtins.code_actions.refactoring.with {
+        filetypes = { "lua", "python", "java", "cpp" },
+      },
     }
     return config -- return final config table
   end,
