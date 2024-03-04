94d8653cd19b447d80f1c6d05162fea0b0a515e3
JustRayCB <rayancbpro@gmail.com>
2024-03-04 11:23:14 +0100

    Dap ensure install does not work so I put it here

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -29,6 +29,13 @@ return {
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         "prettier",
         "stylua",
+        -- "checkstyle",
+        "cpptools",
+        "codelldb",
+        "debugpy",
+        "javadbg",
+        "java-debug-adapter",
+        "java-test",
         -- add more arguments for adding more null-ls sources
       })
     end,
