0d3e7661834de6a56136541116f410c821cc226b
JustRayCB <rayancbpro@gmail.com>
2024-04-05 11:42:35 +0200

    Add config and mappings for telescope media files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,8 +1,21 @@
 return {
   "nvim-telescope/telescope.nvim",
   dependencies = { -- add a new dependency to telescope that is our new plugin
-    "nvim-telescope/telescope-media-files.nvim",
+    "KilloPillers/telescope-media-files.nvim", -- using fork of telescope-media-files cause error with pdf
     "ThePrimeagen/refactoring.nvim",
+    "nvim-lua/popup.nvim",
+    "nvim-lua/plenary.nvim",
+    {
+      "AstroNvim/astrocore",
+      opts = {
+        mappings = {
+          n = {
+            ["<Leader>fi"] = { "<Cmd>Telescope media_files<CR>", desc = "Open in media files" },
+            ["<Leader>rr"] = { "<Cmd>Telescope refactoring refactors<CR>", desc = "Refactor" },
+          },
+        },
+      },
+    },
   },
   -- the first parameter is the plugin specification
   -- the second is the table of options as set up in Lazy with the `opts` key
@@ -15,16 +28,14 @@ return {
     -- load refactoring Telescope extension
     require("telescope").load_extension "refactoring"
 
-    vim.keymap.set({ "n", "x" }, "<leader>rr", function() require("telescope").extensions.refactoring.refactors() end)
-
-    -- opts.extensions = {
-    --   media_files = {
-    --     -- filetypes whitelist
-    --     -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
-    --     filetypes = { "png", "webp", "jpg", "jpeg", "pdf" },
-    --     -- find command (defaults to `fd`)
-    --     find_cmd = "rg",
-    --   },
-    -- }
+    opts.extensions = {
+      media_files = {
+        -- filetypes whitelist
+        -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
+        filetypes = { "png", "jpg", "pdf" },
+        -- find command (defaults to `fd`)
+        find_cmd = "fd",
+      },
+    }
   end,
 }
