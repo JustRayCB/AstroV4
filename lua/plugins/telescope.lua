2caced3b2c8305a2ef739713db46b683a39bbcbb
JustRayCB <rayancbpro@gmail.com>
2024-05-10 17:19:40 +0200

    Remove telescope mediafile extension

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,6 @@
 return {
   "nvim-telescope/telescope.nvim",
   dependencies = { -- add a new dependency to telescope that is our new plugin
-    "KilloPillers/telescope-media-files.nvim", -- using fork of telescope-media-files cause error with pdf
     "ThePrimeagen/refactoring.nvim",
     "nvim-lua/popup.nvim",
     "nvim-lua/plenary.nvim",
@@ -9,10 +8,7 @@ return {
       "AstroNvim/astrocore",
       opts = {
         mappings = {
-          n = {
-            ["<Leader>fi"] = { "<Cmd>Telescope media_files<CR>", desc = "Open in media files" },
-            ["<Leader>rr"] = { "<Cmd>Telescope refactoring refactors<CR>", desc = "Refactor" },
-          },
+          n = {},
         },
       },
     },
@@ -22,20 +18,7 @@ return {
   config = function(plugin, opts)
     -- run the core AstroNvim configuration function with the options table
     require "astronvim.plugins.configs.telescope"(plugin, opts)
-
-    -- require telescope and load extensions as necessary
-    require("telescope").load_extension "media_files"
     -- load refactoring Telescope extension
     require("telescope").load_extension "refactoring"
-
-    opts.extensions = {
-      media_files = {
-        -- filetypes whitelist
-        -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
-        filetypes = { "png", "jpg", "pdf" },
-        -- find command (defaults to `fd`)
-        find_cmd = "fd",
-      },
-    }
   end,
 }
