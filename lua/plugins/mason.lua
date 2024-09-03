d66dc456210171ea095b7e516a81f55704619e75
JustRayCB <rayancbpro@gmail.com>
2024-09-03 17:29:18 +0200

    Modification based on template

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -7,30 +7,31 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
-    opts = function(_, opts)
+    opts = {
       -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+      ensure_installed = {
         "lua_ls",
         "jdtls",
         "clangd",
         "sqlls",
-        "pyright",
+        -- "pyright", -- Using basedpyright instead
         "typst_lsp",
-        "tailwindcss",
-        "tsserver",
-        "cssls",
+        -- "tailwindcss",
+        -- "tsserver",
+        -- "cssls",
         -- add more arguments for adding more language servers
-      })
-    end,
+      },
+    },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
   {
     "jay-babu/mason-null-ls.nvim",
     -- overrides `require("mason-null-ls").setup(...)`
-    opts = function(_, opts)
+    opts = {
+
       -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "prettier",
+      ensure_installed = {
+        -- "prettier",
         "stylua",
         -- "checkstyle",
         "cpptools",
@@ -40,15 +41,15 @@ return {
         "java-debug-adapter",
         "java-test",
         -- add more arguments for adding more null-ls sources
-      })
-    end,
+      },
+    },
   },
   {
     "jay-babu/mason-nvim-dap.nvim",
     -- overrides `require("mason-nvim-dap").setup(...)`
-    opts = function(_, opts)
+    opts = {
       -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+      ensure_installed = {
         -- "python"
         "cpptools",
         "codelldb",
@@ -57,7 +58,7 @@ return {
         "java-debug-adapter",
         "java-test",
         -- add more arguments for adding more debuggers
-      })
-    end,
+      },
+    },
   },
 }
