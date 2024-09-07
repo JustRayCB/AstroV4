c5c225328ecf1b1f924a26e0aa0eb1111ed879d9
JustRayCB <rayancbpro@gmail.com>
2024-09-07 19:16:38 +0200

    remove neotree mapping of wslview

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,17 +1,6 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
-    window = {
-      mappings = {
-        ["O"] = {
-          function(state)
-            local node = state.tree:get_node()
-            vim.fn.execute("silent !wslview '" .. node.path .. "'")
-          end,
-          desc = "System Open",
-        },
-      },
-    },
     filesystem = {
       group_empty_dirs = true,
     },
