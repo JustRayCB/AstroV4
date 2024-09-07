f58ead9010ee8ba5c6552c6b5ba33e3dd7e41b4c
JustRayCB <rayancbpro@gmail.com>
2024-09-07 16:52:58 +0200

    remove html config for now

--- lua/plugins/html.lua
+++ lua/plugins/html.lua
@@ -1,20 +1,21 @@
 return {
-  {
-    "turbio/bracey.vim",
-    build = "pnpm install --prefix server",
-    ft = { "html", "css", "javascript" },
-    init = function()
-      vim.g.bracey_browser_command = "wslview"
-      vim.g.bracey_auto_start_browser = 1
-      vim.g.bracey_refresh_on_save = 1
-      vim.g.bracey_eval_on_save = 1
-      vim.g.bracey_auto_start_server = 1
-      vim.g.bracey_server_allow_remote_connections = 1
-    end,
-  },
-  {
-    "ray-x/web-tools.nvim",
-    build = "pnpm install -g browser-sync",
-    opts = {},
-  },
+  -- {
+  --   "turbio/bracey.vim",
+  --   build = "pnpm install --prefix server",
+  --   ft = { "html", "css", "javascript" },
+  --   init = function()
+  --     vim.g.bracey_browser_command = "xdg-open"
+  --     vim.g.bracey_auto_start_browser = 1
+  --     vim.g.bracey_refresh_on_save = 1
+  --     vim.g.bracey_eval_on_save = 1
+  --     vim.g.bracey_auto_start_server = 1
+  --     vim.g.bracey_server_allow_remote_connections = 1
+  --   end,
+  -- },
+  -- {
+  --   "ray-x/web-tools.nvim",
+  --   ft = { "html", "css", "javascript" },
+  --   build = "pnpm install -g browser-sync",
+  --   opts = {},
+  -- },
 }
