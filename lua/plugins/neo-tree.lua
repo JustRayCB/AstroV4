6ecf9a3ea94a5d00ad6deb09b8ad5cfed60928af
JustRayCB <rayancbpro@gmail.com>
2024-09-07 19:40:45 +0200

    fix neotree rename popup close after entering  normal mode

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -4,5 +4,17 @@ return {
     filesystem = {
       group_empty_dirs = true,
     },
+    event_handlers = {
+      {
+        event = "neo_tree_popup_input_ready",
+        ---@param args { bufnr: integer, winid: integer }
+        handler = function(args)
+          -- map <esc> to enter normal mode (by default closes prompt)
+          -- don't forget `opts.buffer` to specify the buffer of the popup.
+          vim.keymap.set("i", "jj", "<Esc>`^", { noremap = true, buffer = args.bufnr })
+          vim.keymap.set("i", "<esc>", "<Esc>`^", { noremap = true, buffer = args.bufnr })
+        end,
+      },
+    },
   },
 }
