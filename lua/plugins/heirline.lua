806cb25d6d72df451ec40888fe77b4ef703f705a
JustRayCB <rayancbpro@gmail.com>
2024-09-07 22:39:47 +0200

    Nice heirline (code dirty tho)

--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -13,23 +13,60 @@ return {
   },
   opts = function(_, opts)
     local status = require "astroui.status"
-    opts.statusline = { -- statusline
+    local get_icon = require("astroui").get_icon
+    local is_available = require("astrocore").is_available
+    local path_func = status.provider.filename { modify = ":.:h", fallback = "" }
+    opts.statusline = {
       hl = { fg = "fg", bg = "bg" },
-      -- status.component.mode(),
+      -- Show mode text
       status.component.mode {
-        mode_text = { padding = { left = 1, right = 1 } },
-      }, -- add the mode text
-      status.component.git_branch(),
-      status.component.file_info(),
-      status.component.git_diff(),
-      status.component.diagnostics(),
+        -- enable mode text with padding as well as an icon before it
+        mode_text = {
+          icon = { kind = "Mode", padding = { left = 0, right = 1 } },
+        },
+        -- surround the component with a separators
+        surround = {
+          separator = "left",
+        },
+      },
+      status.component.file_info {
+        file_icon = { padding = { left = 0, right = 1 } },
+        filename = { fallback = "Empty" },
+        filetype = false,
+        file_modified = false,
+        surround = {
+          separator = "left",
+        },
+      },
+      status.component.git_branch {
+        padding = { left = 2, right = 1 },
+        surround = {
+          separator = "left",
+        },
+      },
+      -- Add a icon to represent diagnostic and git git diff
+      status.component.builder {
+        provider = function() return get_icon "Diagnostic" .. "/" .. get_icon "Github" end,
+        hl = { fg = "black" },
+        surround = {
+          separator = "left",
+          color = "#c0caf5",
+        },
+      },
+      status.component.diagnostics {
+        surround = {
+          separator = "none",
+          color = "bg",
+        },
+      },
+      status.component.git_diff {
+        padding = { left = 1 },
+        surround = {
+          separator = "none",
+          color = "bg",
+        },
+      },
       status.component.fill(),
-      status.component.cmd_info(),
-      status.component.fill(),
-      status.component.lsp(),
-      status.component.virtual_env(),
-      status.component.treesitter(),
-      status.component.nav(),
       -- Create a custom component to display the time
       status.component.builder {
         {
@@ -55,9 +92,144 @@ return {
           end),
         },
         hl = status.hl.get_attributes "mode", -- highlight based on mode attributes
-        surround = { separator = "right", color = status.hl.mode_bg }, -- background highlight based on mode
+        surround = { separator = "left", color = status.hl.mode_bg }, -- background highlight based on mode
+      },
+      status.component.fill(),
+      -- Show search counts and results
+      {
+        condition = function(self)
+          local query = vim.fn.getreg "/"
+          if query == "" then return false end
+          query = query:gsub([[^\V]], "")
+          query = query:gsub([[\<]], ""):gsub([[\>]], "")
+
+          local search_count = vim.fn.searchcount { recompute = 1, maxcount = -1 }
+          if search_count.total == 0 then return false end
+
+          self.query = query
+          self.count = search_count
+          return true
+        end,
+        status.component.builder {
+          provider = function(self)
+            return status.utils.stylize(" " .. self.query .. " " .. self.count.current .. "/" .. self.count.total, {
+              icon = { kind = "Search" },
+            })
+          end,
+          hl = { fg = "black" },
+          surround = {
+            separator = "left",
+            color = "search_bg",
+          },
+        },
+      },
+      -- Show file encoding
+      -- Show Codeium status
+      status.component.lsp {
+        lsp_client_names = {
+          icon = { kind = "ActiveLSP", padding = { right = 1 } },
+        },
+        surround = { separator = "left" },
+      },
+      status.component.virtual_env(),
+      status.component.builder {
+        provider = function()
+          return status.utils.stylize(string.upper(vim.bo.fileencoding), {
+            icon = { kind = "FileEncoding", padding = { left = 1, right = 1 } },
+          })
+        end,
+        hl = { fg = "text_fg" },
+        padding = { right = 1 },
+      },
+      {
+        status.component.builder {
+          { provider = get_icon "ScrollText" },
+          padding = { right = 1 },
+          -- hl = { fg = "black" },
+          update = { -- update should happen when the mode has changed as well as when the time has changed
+            "User", -- We can use the User autocmd event space to tell the component when to update
+            "ModeChanged",
+            callback = vim.schedule_wrap(function(_, args)
+              if -- update on user UpdateTime event and mode change
+                (args.event == "User") or (args.event == "ModeChanged" and args.match:match ".*:.*")
+              then
+                vim.cmd.redrawstatus() -- redraw on update
+              end
+            end),
+          },
+          hl = status.hl.get_attributes "mode", -- highlight based on mode attributes
+          surround = {
+            separator = { " ", "" },
+            color = status.hl.mode_bg,
+          }, -- background highlight based on mode
+        },
+        status.component.nav {
+          update = { -- update should happen when the mode has changed as well as when the time has changed
+            "User", -- We can use the User autocmd event space to tell the component when to update
+            "ModeChanged",
+            "CursorMoved",
+            "CursorMovedI",
+            "BufEnter",
+            callback = vim.schedule_wrap(function(_, args)
+              if -- update on user UpdateTime event and mode change
+                (args.event == "User")
+                or (args.event == "ModeChanged" and args.match:match ".*:.*")
+                or (args.event == "CursorMoved" or args.event == "CursorMovedI" or args.event == "BufEnter")
+              then
+                vim.cmd.redrawstatus() -- redraw on update
+              end
+            end),
+          },
+          surround = { separator = { "", "" }, color = status.hl.mode_bg },
+          -- hl = status.hl.get_attributes "mode", -- highlight based on mode attributes
+        },
       },
     }
+    -- opts.winbar = {
+    --   -- store the current buffer number
+    --   init = function(self) self.bufnr = vim.api.nvim_get_current_buf() end,
+    --   fallthrough = false, -- pick the correct winbar based on condition
+    --   -- inactive winbar
+    --   {
+    --     condition = function() return not status.condition.is_active() end,
+    --     -- show the path to the file relative to the working directory
+    --     status.component.separated_path { path_func = path_func },
+    --     -- add the file name and icon
+    --     status.component.file_info {
+    --       file_icon = { hl = status.hl.file_icon "winbar", padding = { left = 0 } },
+    --       filename = {},
+    --       filetype = false,
+    --       file_modified = false,
+    --       file_read_only = false,
+    --       hl = status.hl.get_attributes("winbarnc", true),
+    --       surround = false,
+    --       update = "BufEnter",
+    --     },
+    --   },
+    --   -- active winbar
+    --   {
+    --     -- show the path to the file relative to the working directory
+    --     status.component.separated_path { path_func = path_func },
+    --     -- add the file name and icon
+    --     status.component.file_info { -- add file_info to breadcrumbs
+    --       file_icon = { hl = status.hl.filetype_color, padding = { left = 0 } },
+    --       filename = {},
+    --       filetype = false,
+    --       file_modified = false,
+    --       file_read_only = false,
+    --       hl = status.hl.get_attributes("winbar", true),
+    --       surround = false,
+    --       update = "BufEnter",
+    --     },
+    --     -- show the breadcrumbs
+    --     status.component.breadcrumbs {
+    --       icon = { hl = true },
+    --       hl = status.hl.get_attributes("winbar", true),
+    --       prefix = true,
+    --       padding = { left = 0 },
+    --     },
+    --   },
+    -- }
 
     -- Now that we have the component, we need a timer to emit the User UpdateTime event
     -- vim.uv.new_timer():start( -- timer for updating the time
---
+++ lua/plugins/ui.lua
@@ -0,0 +1,110 @@
+-- AstroUI provides the basis for configuring the AstroNvim User Interface
+-- Configuration documentation can be found with `:h astroui`
+-- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
+--       as this provides autocomplete and documentation while editing
+
+---@type LazySpec
+return {
+  "AstroNvim/astroui",
+  ---@type AstroUIOpts
+  opts = {
+    -- change colorscheme
+    -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
+    icons = {
+      -- LSP
+      ActiveLSP = "",
+      ActiveTS = "",
+      LSPLoaded = "",
+      LSPLoading1 = "⠋",
+      LSPLoading2 = "⠙",
+      LSPLoading3 = "⠹",
+      LSPLoading4 = "⠸",
+      LSPLoading5 = "⠼",
+      LSPLoading6 = "⠴",
+      LSPLoading7 = "⠦",
+      LSPLoading8 = "⠧",
+      LSPLoading9 = "⠇",
+      LSPLoading10 = "⠏",
+      -- Git
+      Github = "",
+      GitAdd = "",
+      GitBranch = "",
+      GitChange = "",
+      GitConflict = "",
+      GitDelete = "",
+      GitIgnored = "",
+      GitRenamed = "",
+      GitStaged = "✓",
+      GitUnstaged = "✗",
+      Neogit = "",
+      -- Files
+      Ellipsis = "",
+      DefaultFile = "",
+      FileModified = "",
+      FileReadOnly = "",
+      FoldClosed = "",
+      FoldOpened = "",
+      FolderClosed = "",
+      FolderEmpty = "",
+      FolderOpen = "",
+      -- DAP
+      DapBreakpoint = "",
+      DapBreakpointCondition = "",
+      DapBreakpointRejected = "",
+      DapLogPoint = "",
+      DapStopped = "",
+      -- Diagnostics
+      Diagnostic = "󰒡",
+      DiagnosticError = "",
+      DiagnosticHint = "",
+      DiagnosticInfo = "󰋼",
+      DiagnosticWarn = "",
+      -- Misc
+      Mode = "󰊠",
+      FileEncoding = "",
+      ScrollText = "",
+      TabWidth = "",
+      Search = "",
+      Grapple = "󰓹",
+      Codeium = "󱚝",
+    },
+    status = {
+      separators = {
+        left = { " ", "" }, -- separator for the left side of the statusline
+        right = { " ", "" }, -- separator for the right side of the statusline
+        -- tab = { "", "" },
+      },
+      colors = function(hl)
+        local get_hlgroup = require("astroui").get_hlgroup
+        -- use helper function to get highlight group properties
+        hl.text_fg = "#7aa2f7"
+        hl.insert = "#C1E68C"
+        hl.visual = "#BE98FD"
+        hl.replace = "#ea6962"
+        hl.terminal = "#a89984"
+        hl.file_info_fg = "black"
+        hl.file_info_bg = "#7dcfff"
+        hl.git_branch_fg = "#3b4261"
+        hl.git_branch_bg = "#b4f9f8"
+        hl.lsp_fg = "black"
+        hl.lsp_bg = "#9d7cd8"
+        hl.search_bg = "#ffc777"
+        hl.file_encoding_bg = "#f7768e"
+        hl.tab_width_bg = "#ff966c"
+        hl.nav_fg = "black"
+        hl.nav_bg = "#9d7cd8"
+        hl.nav_icon_bg = hl.nav_bg
+        hl.scrollbar = "#3d59a1"
+        return hl
+      end,
+      attributes = {
+        mode = { bold = true },
+        file_info = { bold = true },
+        git_branch = { bold = true },
+        diagnostics = { bold = true },
+        git_diff = { bold = true },
+        lsp = { bold = true },
+      },
+    },
+  },
+}
