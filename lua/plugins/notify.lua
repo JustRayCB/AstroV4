ff3728e93f7820582115bae7c4f7eaab4bc09400
JustRayCB <rayancbpro@gmail.com>
2024-09-07 19:17:03 +0200

    config function to opts function notify

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -1,23 +1,7 @@
 return {
   "rcarriga/nvim-notify",
-  config = function(plugin, opts)
-    require "astronvim.plugins.configs.notify"(plugin, opts)
-    local notify = require "notify"
-    local utils = require "astrocore"
-    local config = {
-      -- stages = "fade_in_slide_out",
-      stages = "slide",
-      timeout = 1000,
-      -- background_colour = "#76b6ef",
-      -- background_colour = "#000000",
-      icons = {
-        ERROR = " ",
-        WARN = " ",
-        INFO = " ",
-        DEBUG = " ",
-        TRACE = " ",
-      },
-    }
-    notify.setup(config)
-  end,
+  opts = {
+    stages = "slide",
+    timeout = 1000,
+  },
 }
