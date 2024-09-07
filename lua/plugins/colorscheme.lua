90381b727b649ca3d832e4f9cad79d72eed60ccc
JustRayCB <rayancbpro@gmail.com>
2024-09-07 16:21:25 +0200

    Config function into opts function tokyonight

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,3 @@
--- if true then return {} end -- REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 ---@type LazySpec
 return {
   {
@@ -12,37 +10,34 @@ return {
   {
     "folke/tokyonight.nvim",
     -- lazy = false,
-    config = function()
-      require("tokyonight").setup {
-
-        styles = {
-          -- Style to be applied to different syntax groups
-          -- Value is any valid attr-list value for `:help nvim_set_hl`
-          comments = { italic = true },
-          keywords = { italic = true },
-          functions = { italic = false },
-          variables = { italic = false },
-          -- Background styles. Can be "dark", "transparent" or "normal"
-          sidebars = "dark", -- style for sidebars, see below
-          floats = "dark", -- style for floating windows
-        },
-        on_highlights = function(highlights, colors)
-          highlights["LspInlayHint"] = {
-            italic = true,
-          }
-          highlights["@conditional"] = { fg = colors.magenta, italic = true }
-          highlights["@repeat"] = { fg = colors.magenta, italic = true }
-          --   highlights["@Comment"] = { fg = colors.red}
-          -- TabLineFill = { link = "StatusLine" },
-          -- LspInlayHint = { style = { "italic" } },
-          highlights["@parameter"] = { fg = colors.cyan, italic = true }
-          -- highlights["@type.builtin"] = { style = {"italic"} },
-          -- highlights["@namespace"] = { style = {"italic"} },
-          -- highlights["@text.uri"] = { style = { "underline" } },
-          -- highlights["@tag.attribute"] = { style = {"italic"} },
-          -- highlights["@tag.attribute.tsx"] = { style = {} },
-        end,
-      }
-    end,
+    opts = {
+      styles = {
+        -- Style to be applied to different syntax groups
+        -- Value is any valid attr-list value for `:help nvim_set_hl`
+        comments = { italic = true },
+        keywords = { italic = true },
+        functions = { italic = false },
+        variables = { italic = false },
+        -- Background styles. Can be "dark", "transparent" or "normal"
+        sidebars = "dark", -- style for sidebars, see below
+        floats = "dark", -- style for floating windows
+      },
+      on_highlights = function(highlights, colors)
+        highlights["LspInlayHint"] = {
+          italic = true,
+        }
+        highlights["@conditional"] = { fg = colors.magenta, italic = true }
+        highlights["@repeat"] = { fg = colors.magenta, italic = true }
+        --   highlights["@Comment"] = { fg = colors.red}
+        -- TabLineFill = { link = "StatusLine" },
+        -- LspInlayHint = { style = { "italic" } },
+        highlights["@parameter"] = { fg = colors.cyan, italic = true }
+        -- highlights["@type.builtin"] = { style = {"italic"} },
+        -- highlights["@namespace"] = { style = {"italic"} },
+        -- highlights["@text.uri"] = { style = { "underline" } },
+        -- highlights["@tag.attribute"] = { style = {"italic"} },
+        -- highlights["@tag.attribute.tsx"] = { style = {} },
+      end,
+    },
   },
 }
