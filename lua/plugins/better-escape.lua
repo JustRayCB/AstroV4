ea34d0aef8f3d4ee8af7fd544a7604510648b318
JustRayCB <rayancbpro@gmail.com>
2024-09-07 16:11:02 +0200

    Move from config to opts function in better-escape plugin

--- lua/plugins/better-escape.lua
+++ lua/plugins/better-escape.lua
@@ -1,30 +1,26 @@
 return {
   {
     "max397574/better-escape.nvim",
-    -- enabled = false
-    config = function(plugin, opts)
-      -- require "plugins.configs.better-escape"(plugin, opts)
-      require("better_escape").setup {
-        timeout = vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
-        -- default_mappings = false, -- weird behavior
-        mappings = {
-          i = {
-            j = {
-              -- These can all also be functions
-              -- j = "<Esc>`^",
-              j = function()
-                vim.api.nvim_input "<Esc>`^"
-                local current_line = vim.api.nvim_get_current_line()
-                if current_line:match "^%s+j$" then vim.schedule(function() vim.api.nvim_set_current_line "" end) end
-              end,
-            },
+    opts = {
+      timeout = vim.o.timeoutlen,
+      mappings = {
+        i = {
+          j = {
+            -- These can all also be functions
+            -- j = "<Esc>`^",
+            j = function()
+              vim.api.nvim_input "<Esc>`^"
+              local current_line = vim.api.nvim_get_current_line()
+              -- This clean the line if there is nothing on it
+              if current_line:match "^%s+j$" then vim.schedule(function() vim.api.nvim_set_current_line "" end) end
+            end,
           },
         },
-        -- example(recommended)
-        -- keys = function()
-        --   return vim.api.nvim_win_get_cursor(0)[2] > 1 and '<esc>l' or '<esc>'
-        -- end,
-      }
-    end,
+      },
+      -- example(recommended)
+      -- keys = function()
+      --   return vim.api.nvim_win_get_cursor(0)[2] > 1 and '<esc>l' or '<esc>'
+      -- end,
+    },
   },
 }
