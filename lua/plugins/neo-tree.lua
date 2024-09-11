cd3363c638217f69e3ef256cc3a02fd40eee5192
JustRayCB <rayancbpro@gmail.com>
2024-09-11 15:00:20 +0200

    dot files visibles

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,6 +2,14 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
     filesystem = {
+      filtered_items = {
+        visible = false,
+        hide_dotfiles = false,
+        hide_by_name = {
+          ".git",
+          "node_modules",
+        },
+      },
       group_empty_dirs = true,
     },
     event_handlers = {
